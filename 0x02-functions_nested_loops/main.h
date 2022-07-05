2276f54c0cfbf4352b2e0f0e2cf7cf173d316581
= <=>
2022-07-05 07:53:35 -0700

    added prototypes to the header file, implemented print_alphabet method

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 void _putchar(char c);
+void times_table(void);
+void print_to_98(int n);
+int add(int, int);
+int print_last_digit(int);
+void jack_bauer(void);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
 
 #endif
