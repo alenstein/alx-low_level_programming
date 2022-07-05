0f542972b400cfe5fe2cbddac0cf844d4e6fd937
= <=>
2022-07-05 08:45:04 -0700

    implemted add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - a function that adds two integers
+* and returns the result.
+*
+* @a: first number.
+* @b: second number.
+*
+* Return: Add of number one and number two.
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
