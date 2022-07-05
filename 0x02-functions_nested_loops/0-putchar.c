1c9c7975fc0514bca8d75c661b0369081095eb44
= <=>
2022-07-05 07:34:12 -0700

    made corrections to task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main - prints _putchar to console
 * Return: always 0
@@ -8,7 +9,7 @@ int main(void)
 	int i = 0;
 	char chars[] = "_putchar";
 	int size = sizeof(chars) / sizeof(chars[0]);
-	
+
 	while (i < size)
 	{
 		_putchar(chars[i]);
