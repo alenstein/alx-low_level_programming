e7fcf0a93d0790f6ef2d3049cd07b88c500eb37e
= <=>
2022-07-09 07:10:37 -0700

    implemented is_upper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * * _isupper - checks for uppercase character
+ * * @c: variable text
+ * * Return: 0 for lowercase, 1 for uppercase.
+ * */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
