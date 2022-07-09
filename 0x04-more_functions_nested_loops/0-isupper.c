c7829f243e4d31272afa98743c84fe231a9b2500
= <=>
2022-07-09 07:12:51 -0700

    cleared betty format error in is_upper function code

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
- * * _isupper - checks for uppercase character
- * * @c: variable text
- * * Return: 0 for lowercase, 1 for uppercase.
- * */
+* _isupper - checks for uppercase character
+* @c: variable text
+* Return: 0 for lowercase, 1 for uppercase.
+*/
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
