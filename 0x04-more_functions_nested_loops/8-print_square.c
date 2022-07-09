6e19ddb905598eafa2886020f05714357de91c56
= <=>
2022-07-09 07:42:49 -0700

    implemented print square function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - function to print a square
+* @length: takes in the length of the square
+*/
+
+void print_square(int length)
+{
+	int i, j;
+
+	if (length <= 0)
+		_putchar('\n');
+	for (i = 0; i < length; i++)
+	{
+		for (j = 0; j < length; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
