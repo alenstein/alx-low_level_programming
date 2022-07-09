88e9e55d0fcf01213833b3ce1fa30fb1d235659c
= <=>
2022-07-09 07:25:01 -0700

    implemented mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - function that multiplies two integers
+* @a: First integer
+* @b: Second integer
+* Return: multiplication result of a and b.
+*/
+
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}
