8742c0e4e9268411be4ffc188960c43213e2c4ad
= <=>
2022-07-09 07:58:36 -0700

    print_triangle funtion implmented

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle
+* @size: takes in a integer for size of triangle
+*/
+
+void print_triangle(int size)
+{
+	int rw, col, a;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (rw = 0; rw < size; rw++)
+	{
+		for (col = size - rw; col > 1; col--)
+			_putchar(' ');
+		for (a = rw + col; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
