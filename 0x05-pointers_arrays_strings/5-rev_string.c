5eac69d3c61e27c5e60108d30a8083d6fe406dfb
= <=>
2022-07-15 09:50:26 -0700

    finished task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_rev - function to print string in reverse.
+* @s: value taken from main
+* Return: (0)
+*/
+void rev_string(char *s)
+{
+	int len = 0;
+	int a = 0;
+	char *b = s;
+	int c = 0;
+	int x;
+	char n;
+
+	while (*b != '\0')
+	{
+		b++;
+		len++;
+	}
+	a = len - 1;
+	for ( ; c < ((l / 2) + 1) ; c++)
+	{
+		x = (a - c);
+		n = s[c];
+		s[c] = s[x];
+		s[x] = n;
+	}
+}
