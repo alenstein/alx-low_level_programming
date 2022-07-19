c13858de3c29378316cca0b8bd51b47b4b3831ef
= <=>
2022-07-19 10:02:31 -0700

    implemented print _chessboard function

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard
+ * @a: array of chars
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+
+	_putchar('\n');
+	}
+}
