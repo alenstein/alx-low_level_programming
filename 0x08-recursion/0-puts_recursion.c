07793a68bdae24b394937bbc78abdc5cbcf8f1ed
= <=>
2022-07-20 09:41:58 -0700

    Wrote a function that prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
 * _puts_recursion - prints a string, followed by a new line
@@ -14,5 +14,6 @@ void _puts_recursion(char *s)
 		return;
 	}
 	_putchar(*s);
+
 	_puts_recursion(s + 1);
 }
