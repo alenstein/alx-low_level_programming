93a052ad24f1903182256d3e1d902bcae9c8a19a
= <=>
2022-07-23 06:51:30 -0700

    made corrections for task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,14 +1,32 @@
 #include "main.h"
+
+/**
+ * checkIf_prime - checks if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+
+int checkIf_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (checkIf_prime(n, i - 1));
+}
+
 /**
  * is_prime_number - checks if a given number is prime
  * @n: given number
  * Return: 1 if number is prime else 0
  **/
+
 int is_prime_number(int n)
 {
-	if (n <= 1 || _sqrt_recursion(n) >= 1)
+	if (n <= 1)
 		return (0);
-	if (_sqrt_recursion(n) == -1)
-		return (1);
-	return (_sqrt_recursion(n));
+	return (checkIf_prime(n, n - 1));
+
 }
