73c29008365aa8e50f2d212e437e0634c3162ae4
= <=>
2022-07-25 03:31:11 -0700

    restored .c files for the libmy.a library

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - prints _putchar to console
+* Return: always 0
+*/
+int main(void)
+{
+	int i = 0;
+	char chars[] = "_putchar";
+	int size = 8;
+
+	while (i < size)
+	{
+		_putchar(chars[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
