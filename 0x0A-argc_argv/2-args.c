b9d61f3a5819d00b6c1fd9369e6fb3595057f7e7
= <=>
2022-07-27 06:27:16 -0700

    finished task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - All arguments should be printed, including the first one
+ * Only prints one argument per line, ending with a new line
+ * @argc: number of commandline arguments
+ * @argv: array of commandline arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
