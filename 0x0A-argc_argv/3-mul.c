891d506527e06e9809910af35c435f6c8b6b794c
= <=>
2022-07-27 06:42:44 -0700

    finished task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two 2 given numbers
+ * @argc: number of commandline arguments
+ * @argv: array of commandline arguments
+ * Return: 0 on success, 1 for Error
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, ans;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	ans = a * b;
+	printf("%d \n", ans);
+
+	return (0);
+}
